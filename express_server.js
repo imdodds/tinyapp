ddb1140709f089d042b20307d63649e666fc9b78
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-26 21:16:38 +0000

    Added Edit buttons, form and POST route to update a long URL

--- express_server.js
+++ express_server.js
@@ -32,40 +32,55 @@ const addNewURL = (longURL) => {
   return newID;
 };
 
+// Root directory
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Diplay urlDatabse
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// User submits new URL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Add new URL to urlDatabase
 app.post("/urls", (req, res) => {
   let newID = addNewURL(req.body.longURL);
   const templateVars = { id: newID, longURL: urlDatabase[newID] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
+// Display long and short URLs
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+// Redirect to Long URL
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id]
   res.redirect(longURL)
 });
 
+//Edit a URL
+app.post("/urls/:id", (req, res) => {
+  const longURL = req.body.longURL;
+  urlDatabase[req.params.id] = longURL;
+  res.redirect("/urls");
+});
+
+// Delete a URL
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 })
 
+/*
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -73,7 +88,9 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+*/
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
