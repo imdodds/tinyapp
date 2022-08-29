f350704094737afc5bf884c3aab58c0ca9d308f7
Ian Dodds <ianmitchelldodds@gmail.com>
2022-08-29 18:39:57 +0000

    Added functionality for user login/logout

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
@@ -43,28 +44,48 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+//User logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 // Diplay urlDatabse
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 // User submits new URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
+  res.render("urls_new", templateVars);
 });
 
 // Add new URL to urlDatabase
 app.post("/urls", (req, res) => {
   let newID = addNewURL(req.body.longURL);
-  const templateVars = { id: newID, longURL: urlDatabase[newID] };
+  const templateVars = { 
+    id: newID,
+    longURL: urlDatabase[newID],
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
-  // res.redirect("/urls");
 });
 
 // Display long and short URLs
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
 });
 
